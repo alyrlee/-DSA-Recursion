69319a1cbb3bb30053498c6cd02fafb3d692f617
Ashley Lee <53842786+alyrlee@users.noreply.github.com>
2020-09-03 13:39:13 -0400

    Update data-structures-recursion.js

--- data-structures-recursion.js
+++ data-structures-recursion.js
@@ -216,6 +216,10 @@ function organize(org, boss) {
 console.log(JSON.stringify(organize(org, null)));
 
 /* ======== 12. Binary Representation =================
+Write a recursive function that prints out the binary representation of a given number.
+For example, the program should take 3 as an input and print 11 as output, or 25 as an input and print 11001 as an output. 
+Note that the binary representation of 0 should be 0.
 =============================================== */
+
 
 
